98d47058e5894bbcbf44a4bcfa11365a11c549e5
Kauã <kau.lira@yahoo.com>
2022-04-08 19:30:13 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int idade; 
+
+Console.Write("Indique sua idade: ");
+idade = Convert.ToInt32(Console.ReadLine());
+
+if(idade >= 18 && idade <= 67)
+{   
+  Console.WriteLine("Você pode doar sangue");
+}
+
+else
+{   
+   Console.WriteLine("Você não pode doar sangue");  
+}
